dd53234a59fe6b850d37a20b3e5e40d366daab58
dkyw <derrellwong@gmail.com>
2017-06-08 20:23:32 +0000

    rememeber user if registering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, Notice: 'Thank you for signing up!'
     else
       render :new
@@ -16,11 +17,12 @@ class UsersController < ApplicationController
  private
   def user_params
     params.require(:user).permit(
-      :name,
       :first_name,
       :last_name,
+      :email,
       :password,
       :password_confirmation
       )
   end
+
 end
