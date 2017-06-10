9f0f14bad3de2eb85d9bec4d6e3a7edf7800161e
dkyw <derrellwong@gmail.com>
2017-06-10 19:29:11 +0000

    added class for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,14 +7,11 @@ class ReviewsController < ApplicationController
     @review.product_id = params[:product_id]
     @review.user = current_user
 
-    # Another option, but results in `no such method whales for nil:NilClass` if the alien doesn't exist
-    # @alien = Alien.find(params[:alien_id])
-    # @whale = @alien.whales.new(whale_params)
 
     if @review.save
-      redirect_to @review.product, notice: 'Whale was successfully created.'
+      redirect_to @review.product, notice: 'Review was successfully created.'
     else
-      redirect_to @review.product, notice: 'Failed to create the whale'
+      redirect_to @review.product, notice: 'Failed to create the review'
     end
   end
 
