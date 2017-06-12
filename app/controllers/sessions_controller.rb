254bf16ed51748e33b9efe4a6a92a2433a610783
dkyw <derrellwong@gmail.com>
2017-06-12 21:00:27 +0000

    added authenticate_with_credentials fn to verify credentials on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
@@ -13,6 +12,17 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+
+    # user = User.find_by_email(params[:email])
+    # if user && user.authenticate(params[:password])
+    #   session[:user_id] = user.id
+    #   redirect_to root_url, notice: "Logged in!"
+    # else
+    #   flash.now[:error] = "Email or password incorrect"
+    #   render :new
+    # end
+
+
 
   def destroy
     session[:user_id] = nil
