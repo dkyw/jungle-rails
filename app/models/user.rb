24040fd5e2cb027785acf1b7a63240a42a06d91b
dkyw <derrellwong@gmail.com>
2017-06-12 21:00:53 +0000

    added authenticate_with_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,24 @@
 class User < ActiveRecord::Base
 
- has_secure_password
+  has_secure_password
 
- has_many :reviews
+  has_many :reviews
 
- validates :first_name, presence: true
- validates :last_name, presence: true
- validates :email, uniqueness: true
- validates :password, confirmation: true
- validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+
+
+  validates :password, confirmation: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+
+  def self.authenticate_with_credentials(email, pw)
+    # strip removes all spaces in email
+    email_stripped_dc = email.strip.downcase
+    user = User.find_by_email(email_stripped_dc)
+    user && user.authenticate(pw) ? user : nil
+  end
 end
+
